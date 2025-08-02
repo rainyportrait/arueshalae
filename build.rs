a94d470ebfb058372b5f0f479f2c30f637f47c06
Rian Portrait <rainyportrait@protonmail.com>
2025-08-02 13:08:46 +0200

    install npm packages during build step

--- build.rs
+++ build.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context, Result, bail};
 use camino::Utf8Path;
 use tokio::process::Command;
 
@@ -5,39 +6,59 @@ use tokio::process::Command;
 mod database;
 
 #[tokio::main(flavor = "current_thread")]
-async fn main() {
+async fn main() -> Result<()> {
     let version = env!("CARGO_PKG_VERSION");
+    install_npm_dependencies().await?;
 
     let migrate_db_future = create_dev_database();
     let build_userscript_future = build_userscript(version);
     let build_tailwind_future = build_tailwind();
 
-    tokio::join!(
+    tokio::try_join!(
         migrate_db_future,
         build_userscript_future,
         build_tailwind_future
-    );
+    )?;
+
+    Ok(())
 }
 
-async fn create_dev_database() {
+async fn create_dev_database() -> Result<()> {
     let path = Utf8Path::new("./dev.db");
-    _ = tokio::fs::remove_file(&path).await;
+    tokio::fs::remove_file(&path).await?;
     let _database = database::Database::new(path)
         .await
-        .expect("create dev database");
+        .context("failed to migrate database for sqlx macro checking")?;
+    Ok(())
 }
 
-async fn build_userscript(version: &str) {
-    Command::new("node")
+async fn install_npm_dependencies() -> Result<()> {
+    let status = Command::new("npm")
+        .arg("install")
+        .status()
+        .await
+        .context("failed to start npm install command")?;
+    if !status.success() {
+        bail!("npm install failed")
+    }
+    Ok(())
+}
+
+async fn build_userscript(version: &str) -> Result<()> {
+    let status = Command::new("node")
         .arg("build_userscript")
         .arg(version)
         .status()
         .await
-        .expect("build userscript");
+        .context("could not start node to build userscript")?;
+    if !status.success() {
+        bail!("failed to build userscript")
+    }
+    Ok(())
 }
 
-async fn build_tailwind() {
-    Command::new("npx")
+async fn build_tailwind() -> Result<()> {
+    let status = Command::new("npx")
         .args([
             "@tailwindcss/cli",
             "-i",
@@ -47,5 +68,9 @@ async fn build_tailwind() {
         ])
         .status()
         .await
-        .expect("build tailwind stlyes");
+        .context("failed to start tailwind cli to build tailwind stlyes")?;
+    if !status.success() {
+        bail!("failed to build tailwind")
+    }
+    Ok(())
 }

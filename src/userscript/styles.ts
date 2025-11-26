4bcbe710f97cb59903dae29df1c51cf684576a8d
Rian Portrait <rainyportrait@protonmail.com>
2025-11-26 22:07:16 +0100

    move inserted styles to the end of the document

--- src/userscript/styles.ts
+++ src/userscript/styles.ts
@@ -4,4 +4,4 @@ const STYLE_SHEET_NAME = "arueshalae_styles"
 const style = document.createElement("style")
 style.id = STYLE_SHEET_NAME
 style.innerHTML = styles
-document.head.appendChild(style)
+document.body.appendChild(style)
